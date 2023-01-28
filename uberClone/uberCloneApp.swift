2045f72bbb54a9cc1bf907941a4b79d21f588a56
Bowen1248 <bowenlee31@gmail.com>
2023-01-28 17:38:58 +0800

    Initial Commit

---
+++ uberClone/uberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  uberCloneApp.swift
+//  uberClone
+//
+//  Created by hsnl on 2023/1/28.
+//
+
+import SwiftUI
+
+@main
+struct uberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
